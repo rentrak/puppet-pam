6aba5de4b98e2202003e67f3b661862f1de35e02
Cliff Wells <cliff.wells@rentrakmail.com>
2014-04-01 11:53:52 -0700

    system-auth should be symlink to system-auth-ac

--- manifests/pamd/redhat.pp
+++ manifests/pamd/redhat.pp
@@ -13,14 +13,16 @@ class pam::pamd::redhat {
 
   file { "${pam::params::prefix_pamd}/system-auth-ac":
     content => template('pam/pam.d/system-auth-ac.erb')
-  }
+  } ->
 
   file { "${pam::params::prefix_pamd}/system-auth":
-    content => template('pam/pam.d/system-auth-ac.erb')
+    ensure => 'link',
+    target => "${pam::params::prefix_pamd}/system-auth-ac",
+    force  => versioncmp($::operatingsystemmajrelease, 4) > 0
   }
 
   if($pam::pamd::pam_ldap) {
-    
+
     #File['/etc/ldap.conf'] -> File[$pam::params::ldap_conf]
 
     file { '/etc/ldap.conf':
@@ -35,15 +37,15 @@ class pam::pamd::redhat {
     5 : {}
 
     6 : {
-      
+
       file { "${pam::params::prefix_pamd}/password-auth-ac":
         ensure  => present,
         content => template('pam/pam.d/system-auth-ac.erb'),
       }
-      
+
       # rhel 6 uses pam_ldap.conf:
       if($pam::pamd::pam_ldap) {
-      
+
         #File['/etc/pam_ldap.conf'] -> File[$pam::params::ldap_conf]
 
         file { '/etc/pam_ldap.conf':
@@ -52,7 +54,7 @@ class pam::pamd::redhat {
         }
 
       }
-      
+
     }
 
     default : {
